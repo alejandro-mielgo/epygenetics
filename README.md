1cdcc28467eed5c85b45cb4095f4b3be291a7544
Alejandro <alejandro.mielgo.barba@gmail.com>
2025-05-30 09:52:19 +0200

    edit readme and examples

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
-# Genetic Algorithm
+## Genetic Algorithm
 
 Genetic algorithm for function optimization using numpy.
 
+Install:
+
+```pip install epygenetics```
+
 ### Format to define target function:
 
-$$ f(x,y) = (x+2y-7)^2 +2(x+y-5)^2 $$
+```math
+ f(x,y) = (x+2y-7)^2 +2(x+y-5)^2
+```
 would be defined as 
-
 
 ```python
 def booth(x:np.ndarray)->np.ndarray:
     return -((x[:,0] + 2*x[:,1] - 7)**2 + (2*x[:,0] + x[:,1] -5)**2)
 ```
+
+see the examples folder for more details
 
 ### Crossover methods:
 - one point crossover
--- examples/example_genetic.py
+++ examples/example_genetic.py
@@ -2,18 +2,18 @@ from epygenetics import genetic_optimize
 from epygenetics import rastrigin, booth, himmelblau
 
     
-p : dict = {
+parameters : dict = {
     "n_generations"  : 100,
-    "lower_bound" : (-5.12,-5.12),
-    "upper_bound"  : ( 5.12, 5.12),
+    "lower_bound" : (-5.12,-5.12),  # lower limits for each variable
+    "upper_bound"  : ( 5.12, 5.12), # upper limits for each variable
     "pop_size"  : 100,
-    "mutation_rate" : 0.5,      # how ofter a gene mutates
-    "mutation_n_stdevs" : 1,    # how much it mutates in variable stdvs 
-    "crossover_method" : "tpc", # one point crossover or two point crossover
+    "mutation_rate" : 0.5,          # how ofter a gene mutates
+    "mutation_n_stdevs" : 1,        # how much it mutates in variable stdvs 
+    "crossover_method" : "tpc",     # one point crossover or two point crossover
     "minimize": True
 }
 
 if __name__ == "__main__":
-    _,_,history = genetic_optimize(param=p, target_function=rastrigin)
+    best_indidual,best_solution,history = genetic_optimize(param=parameters, target_function=rastrigin)
     
 
